9d5a4026dab8d04568cd062e24ee756a85568487
kailee <kailee@KAILEEHAMRE67B3>
2024-08-24 02:35:30 -0400

    Add project files.

---
+++ c969v2/Models/Country.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c969v2.Models
+{
+    internal class Country
+    {
+    }
+}
---
+++ c969v2/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c969v2.Models
+{
+    internal class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool Active { get; set; }
+        public DateTime DateTime { get; set; }
+        public string CreatedBy { get; set; }
+        public string LastUpdate { get; set; }
+        public string LastUpdatedBy { get; set; }
+
+    }
+}
